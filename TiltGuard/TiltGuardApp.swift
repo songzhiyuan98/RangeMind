e9651ee0931f05fe1ce04fa6013383dc3cfa51c2
Zhiyuan Song <songzhiyuan98@gmail.com>
2026-03-06 07:46:13 -0800

    Initial Commit

---
+++ TiltGuard/TiltGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  TiltGuardApp.swift
+//  TiltGuard
+//
+//  Created by Zhiyuan Song on 3/6/26.
+//
+
+import SwiftUI
+
+@main
+struct TiltGuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
